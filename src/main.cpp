faf794005c19488c35faa95004dce09b5edf36ef
aymericbarbot <aymeric.barbot@neusta.fr>
2023-11-21 15:37:02 +0100

    avancelent humidex et aws

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,96 @@
-#include <Arduino.h>
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+
 #include <Wire.h>
-#include <SD.h>    // Inclure la bibliothèque SD
 #include "ATH20.h"
+
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+
+void messageHandler(char* topic, byte* payload, unsigned int length);
 
 ATH20 ATH;
 
-File logFile;  // Déclaration d'une variable de fichier pour le journal
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
+
+float humi ;
+float temp;
+
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println("Connecting to Wi-Fi");
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+ 
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.setServer(AWS_IOT_ENDPOINT, 8883);
+ 
+  // Create a message handler
+  client.setCallback(messageHandler);
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    delay(100);
+  }
+ 
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+ 
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
+}
+
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["humidity"] = humi;
+  doc["temperature"] = temp;
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+ 
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+void messageHandler(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+  Serial.println(message);
+}
 
 void setup()
 {
     Serial.begin(115200);
     Serial.println("ATH20 DEMO");
     ATH.begin();
-
-    // Initialisation de la carte SD
-    if (SD.begin(10)) {
-        Serial.println("SD card is ready.");
-    } else {
-        Serial.println("SD card initialization failed.");
-    }
 }
 
 void loop()
@@ -26,31 +98,19 @@ void loop()
     float humi, temp;
     
     int ret = ATH.getSensor(&humi, &temp);
-    
+
     if(ret)     // GET DATA OK
     {
         Serial.print("humidity: ");
         Serial.print(humi*100);
         Serial.print("%\t temerature: ");
         Serial.println(temp);
-
-    
-        // Ouvrir le fichier en mode écriture (créer le fichier s'il n'existe pas)
-        File logFile = SD.open("C:\\Users\\Aymeric Barbot\\Documents\\PlatformIO\\Projects\\humidex\\log\\data.txt", FILE_WRITE);
-
-        // Écrire les données dans le fichier
-        logFile.print("humidity: ");
-        logFile.print(humi * 100);
-        logFile.print("%\t temperature: ");
-        logFile.println(temp);
-
-        // Fermer le fichier
-        logFile.close();
     }
     else        // GET DATA FAIL
     {
         Serial.println("GET DATA FROM ATH20 FAIL");
     }
-    
-    delay(1000);
+    publishMessage();
+    client.loop();
+    delay(100);
 }
